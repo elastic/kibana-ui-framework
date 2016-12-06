5ec0a716710b26e07013e49eeef172a15be78d39
CJ Cenizal <cj@cenizal.com>
2016-12-06 11:01:29 -0800

    Refactor example HTML pages to live at the root of the example directory.

--- src/guide/views/button/button_example.jsx
+++ src/guide/views/button/button_example.jsx
@@ -10,48 +10,48 @@ export default createExample([{
   description: (
     <p>Use the default Button in most situations.</p>
   ),
-  html: require('./button/button_default.html'),
+  html: require('./button_default.html'),
   hasDarkTheme: false,
 }, {
   title: 'Primary Button',
   description: (
     <p>Use the primary Button to represent the most common action. Generally, there won't be a need to present more than one of these at a time.</p>
   ),
-  html: require('./button/button_primary.html'),
+  html: require('./button_primary.html'),
   hasDarkTheme: false,
 }, {
   title: 'Danger Button',
   description: (
     <p>Danger Buttons represent irreversible, potentially regrettable actions.</p>
   ),
-  html: require('./button/button_danger.html'),
+  html: require('./button_danger.html'),
   hasDarkTheme: false,
 }, {
   title: 'Button with icon',
   description: (
     <p>You can toss an icon into a Button, with or without text.</p>
   ),
-  html: require('./button/button_with_icon.html'),
+  html: require('./button_with_icon.html'),
   hasDarkTheme: false,
 }, {
   title: 'ButtonGroup',
   description: (
     <p>Use the ButtonGroup to emphasize the relationships between a set of Buttons, and differentiate them from Buttons outside of the set.</p>
   ),
-  html: require('./button/button_group.html'),
+  html: require('./button_group.html'),
   hasDarkTheme: false,
 }, {
   title: 'In ToolBar',
   description: (
     <p>This example verifies that Buttons are legible against the ToolBar's background.</p>
   ),
-  html: require('./button/buttons_in_tool_bar.html'),
+  html: require('./buttons_in_tool_bar.html'),
   hasDarkTheme: false,
 }, {
   title: 'Element variations',
   description: (
     <p>You can create a Button using a button element, link, div, span, or input[type="submit"].</p>
   ),
-  html: require('./button/button_elements.html'),
+  html: require('./button_elements.html'),
   hasDarkTheme: false,
 }]);
